b4a9b3480ccb61779b314760e5767307eb2aa920
sudarmono <30992530+monoland@users.noreply.github.com>
2024-07-16 16:32:54 +0700

    update

--- src/Providers/SystemServiceProvider.php
+++ src/Providers/SystemServiceProvider.php
@@ -2,9 +2,14 @@
 
 namespace Module\System\Providers;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Fortify;
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 use Module\System\Models\SystemPersonalAccessToken;
 
 class SystemServiceProvider extends ServiceProvider
@@ -16,11 +21,21 @@ class SystemServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Sanctum::usePersonalAccessTokenModel(SystemPersonalAccessToken::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+
+            return Limit::perMinute(5)->by($throttleKey);
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
         Gate::guessPolicyNamesUsing(function ($modelClass) {
             return str($modelClass)->before('\\Models\\')->toString() . '\\Policies\\' . str($modelClass)->after('\\Models\\')->toString() . 'Policy';
         });
-
-        Sanctum::usePersonalAccessTokenModel(SystemPersonalAccessToken::class);
 
         $this->loadViewsFrom(
             __DIR__ . '/../../resources/views',
