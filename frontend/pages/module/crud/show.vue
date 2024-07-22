23b0b9206686d85e2e7d89d71086389702410d2e
monoland <mono.socialmedia@gmail.com>
2024-07-22 10:07:22 +0700

    update

--- frontend/pages/module/crud/show.vue
+++ frontend/pages/module/crud/show.vue
@@ -102,15 +102,61 @@
 			<div class="text-overline mt-4">Aksi</div>
 			<v-divider></v-divider>
 
-			<v-btn
-				class="mt-3"
-				:color="theme"
-				:loading="updateLoading"
-				block
-				variant="flat"
-				@click="checkForUpdate(record)"
-				>CHECK FOR UPDATE</v-btn
+			<v-card-text
+				:class="`bg-${theme}-lighten-5 rounded-lg mt-3`"
+				v-if="updateChecked"
 			>
+				<p v-if="updateStatus"><strong>Update Is Available</strong></p>
+				<p v-else>
+					<strong>module {{ record.name }} is up to date.</strong>
+				</p>
+
+				<div class="mt-2">
+					<p class="text-caption">update version: v1.0.1</p>
+					<p class="text-caption">current version: v1.0.0</p>
+				</div>
+			</v-card-text>
+
+			<v-row dense>
+				<template v-if="updateChecked && updateStatus">
+					<v-col cols="6">
+						<v-btn
+							class="mt-3"
+							:color="`${theme}-lighten-4`"
+							block
+							variant="flat"
+							@click="cancelUpdate(record)"
+							>Cancel</v-btn
+						>
+					</v-col>
+
+					<v-col cols="6">
+						<v-btn
+							class="mt-3"
+							:color="theme"
+							block
+							variant="flat"
+							@click="processUpdate(record)"
+							>Update</v-btn
+						>
+					</v-col>
+				</template>
+
+				<v-col
+					cols="12"
+					v-else
+				>
+					<v-btn
+						class="mt-3"
+						:color="theme"
+						:loading="updateLoading"
+						block
+						variant="flat"
+						@click="checkForUpdate(record)"
+						>CHECK FOR UPDATE</v-btn
+					>
+				</v-col>
+			</v-row>
 
 			<div class="text-overline mt-4">Link</div>
 			<v-divider></v-divider>
@@ -142,13 +188,20 @@ export default {
 
 	data: () => ({
 		updateLoading: false,
+		updateStatus: false,
+		updateChecked: false,
 	}),
 
 	methods: {
 		checkForUpdate: function (record) {
 			this.updateLoading = true;
 
-			this.$http(`system/api/module/${record.id}/check-for-update`);
+			this.$http(`system/api/module/${record.id}/check-for-update`).then(
+				() => {
+					this.updateLoading = false;
+					this.updateChecked = true;
+				}
+			);
 			//
 		},
 	},
